3e7014037b4c17c0f5cd2a5ad67deaf4036fdd6a
Rediet Abreham <nara1415karol@gmail.com>
2025-06-06 19:44:00 +0300

    adding header file

--- alx-project-0x02/pages/index.tsx
+++ alx-project-0x02/pages/index.tsx
@@ -1,10 +1,17 @@
-import Head from "next/head";
+import Head from 'next/head';
+import Header from '@/components/layout/Header';
 
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>ALX Project</title>
+        </Head>
+        <Header />
+        <meta name="description" content="A simple Next.js project with Tailwind CSS" />
+        <link rel="icon" href="/favicon.ico" />
       <main className="p-6">
-        <h1 className="text-2xl font-bold">Hello, welcome to your Next.js + Tailwind project!</h1>
+        <h1 className="text-2xl font-bold">Hello, welcome to  Next.js + Tailwind project!</h1>
       </main>
     </div>
   );
